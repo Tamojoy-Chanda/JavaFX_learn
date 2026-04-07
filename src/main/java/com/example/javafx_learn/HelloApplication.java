fa10a7bcd2bb548d233541ef3dd214c2877b8c3c
Tamojoy Chanda <tamojoychanda@gmail.com>
2026-04-07 18:28:54 +0530

    pane and circle learned

--- src/main/java/com/example/javafx_learn/HelloApplication.java
+++ src/main/java/com/example/javafx_learn/HelloApplication.java
@@ -6,6 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
 
@@ -16,7 +19,7 @@ public class HelloApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
-        stage.show();
+//        stage.show();
 
         StackPane sPane = new StackPane();
         sPane.getChildren().add(new Button("Stack Pane Button"));
@@ -24,6 +27,20 @@ public class HelloApplication extends Application {
         Stage st = new Stage();
         st.setTitle("Experimenting with Stack pane");
         st.setScene(sc);
-        st.show();
+//        st.show();
+
+        Circle circle = new Circle();
+        circle.setCenterX(250);
+        circle.setCenterY(250);
+        circle.setRadius(50);
+        circle.setStroke(Color.LAVENDER);
+        circle.setFill(Color.CORAL);
+        Pane p = new Pane();
+        p.getChildren().add(circle);
+        Scene sc1 = new Scene(p,500,500);
+        Stage stage2 = new Stage();
+        stage2.setScene(sc1);
+        stage2.setTitle("Learning Pane and Circle");
+        stage2.show();
     }
 }
