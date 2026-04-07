93ae6462753a3506a45aeb001a24f19c9655d475
Tamojoy Chanda <tamojoychanda@gmail.com>
2026-04-07 17:10:40 +0530

    Stack pane learned

--- src/main/java/com/example/javafx_learn/HelloApplication.java
+++ src/main/java/com/example/javafx_learn/HelloApplication.java
@@ -3,7 +3,9 @@ package com.example.javafx_learn;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
 
@@ -15,5 +17,13 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        StackPane sPane = new StackPane();
+        sPane.getChildren().add(new Button("Stack Pane Button"));
+        Scene sc = new Scene(sPane,440,320);
+        Stage st = new Stage();
+        st.setTitle("Experimenting with Stack pane");
+        st.setScene(sc);
+        st.show();
     }
 }
